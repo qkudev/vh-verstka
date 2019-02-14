1f525ba76f0f8f7951a896fbe4f3b2d3d3e90494
agusev <devil201053@gmail.com>
2019-02-14 13:50:19 +0700

    slick

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 import $ from 'jquery'
 import popper from 'popper.js'
+import bootstrap from 'bootstrap'
 import '../scss'
+import slick from 'slick-carousel';
+
+$(document).ready(function(){
+  $('.slick-videos').slick({
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  })
+});
 
 window.$ = $
 window.popper = popper
