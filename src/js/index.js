46172111ecd6673f53b04a3bb0094139ae93564c
qkudev <qkud.ev@gmail.com>
2019-02-14 10:26:31 +0700

    html commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import popper from 'popper.js'
 import bootstrap from 'bootstrap'
+import style from '../scss'
 
 window.$ = $
 window.propper = propper
--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,60 @@ module.exports = {
   entry: [
     './src/js/index.js',
     './src/scss/styles.scss',
+    './src/html/index.html'
   ],
   output: {
     filename: './js/bundle.js'
   },
   devtool: "source-map",
   module: {
-    rules: [{
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src/js'),
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['env']
-        }
-      }
-    },
-    {
-      test: /\.(sass|scss)$/,
-      include: path.resolve(__dirname, 'src/scss'),
-      use: ExtractTextPlugin.extract({
-        use: [
-        {
-          loader: 'css-loader',
-        },
-        {
-          loader: 'sass-loader',
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        use: {
+          loader: 'babel-loader',
           options: {
-            sourceMap: true
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        include: path.resolve(__dirname, 'src/scss'),
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'xxx/'
           }
         }]
-      })
-    }
+      },
+      {
+        test: /\.html$/,
+        use: [ {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }],
+      }
     ]
   },
   plugins: [
