e0bd4bcb54c6cafe863a8af39ac2ca89d0a8fef9
qkudev <qkud.ev@gmail.com>
2019-02-15 01:25:23 +0700

    slick-arrow buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,7 @@
 import $ from 'jquery'
 import popper from 'popper.js'
 import '../scss'
-import slick from 'slick-carousel'
-
-$(document).ready(function () {
-  $('.slick-videos').slick({
-    infinite: false,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    variableWidth: true
-  })
-})
-
-$(document).ready(function () {
-  $('.slick-channels').slick({
-    infinite: false,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    variableWidth: true
-  })
-})
-
-$(document).ready(function () {
-  $('.slick-categories')
-    .slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      variableWidth: true,
-    })
-})
-
-$(document).ready(function () {
-  $('.slick-popular')
-    .slick({
-      infinite: true,
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      variableWidth: true,
-    })
-})
+import './slicks'
 
 window.$ = $
 window.popper = popper
---
+++ src/js/slicks.js
@@ -0,0 +1,44 @@
+import slick from 'slick-carousel'
+import $ from 'jquery'
+
+$(document).ready(function () {
+  $('.slick-videos').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true,
+    prevArrow: '<button class="slick-prev slick-arrow slick-arrow-popular" aria-label="Previous" type="button"></button>',
+    nextArrow: '<button class="slick-next slick-arrow slick-arrow-popular" aria-label="Next" type="button"></button>',
+  })
+})
+
+$(document).ready(function () {
+  $('.slick-channels').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    variableWidth: true
+  })
+})
+
+$(document).ready(function () {
+  $('.slick-categories')
+    .slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      variableWidth: true,
+      prevArrow: '<button class="slick-prev slick-arrow slick-arrow-category" aria-label="Previous" type="button"></button>',
+      nextArrow: '<button class="slick-next slick-arrow slick-arrow-category" aria-label="Next" type="button"></button>',
+    })
+})
+
+$(document).ready(function () {
+  $('.slick-popular')
+    .slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      variableWidth: true,
+    })
+})
